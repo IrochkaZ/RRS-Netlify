55c2f40c79171f7f9e33997a10419a454cb2a459
Irina <zaletk.irina@gmail.com>
2020-03-17 22:20:03 +0300

    update v3

---
+++ datagrid/precache-manifest.2656eb00a8cd300bbe69bdc4d6e8b0aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5d2f08baaf38e150b7b92048e4baf3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d6f0ccb9b3ee2d077bba",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4e3f0967600be7935c11",
+    "url": "./static/css/main.02a72245.chunk.css"
+  },
+  {
+    "revision": "d6f0ccb9b3ee2d077bba",
+    "url": "./static/js/2.261c0e91.chunk.js"
+  },
+  {
+    "revision": "1ba69035e6edbd45d0f6e0853d2a9b22",
+    "url": "./static/js/2.261c0e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3f0967600be7935c11",
+    "url": "./static/js/main.a63a48d9.chunk.js"
+  },
+  {
+    "revision": "15bd71826fd82f232173",
+    "url": "./static/js/runtime-main.f6113c09.js"
+  }
+]);
