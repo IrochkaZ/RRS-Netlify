a9b9d3f6672f33376815018316e5e010c7ef7345
Iryna  Zaletko <43729239+IrochkaZ@users.noreply.github.com>
2021-04-01 13:22:59 +0300

    Create loader.js

---
+++ data/loader.js
@@ -0,0 +1,28 @@
+
+(function (name, selector) {
+    const URL = 'https://zaochnik1.com/static/public/', d = document,
+        nam = (name === 'shortAndLong') ? ['short', 'long'] : [name.toLowerCase()],
+        sel = selector.replace(/\s+/g, '').split(','), script = [], link = [], c = [];
+    nam.map((n) => {
+        const data = {
+            links: [`${URL}0.min.css`, `${URL}client_order_public_${n === 'short' ? 'short_blog_' : ''}iframe.min.css`],
+            scripts: [`${URL}vuejs.min.js`, `${URL}client_order_public_${n === 'short' ? 'short_blog_' : ''}iframe.min.js`],
+            component: `<order-public-${n === 'short' ? 'short-' : ''}landing></order-public-${n === 'short' ? 'short-' : ''}landing>`
+        };
+        data.links.map(l => link.push(l));
+        data.scripts.map(s => script.push(s));
+        c.push(data.component);
+    })
+    const a = [...new Set(script)], l = [...new Set(link)];
+    l.map(e => {
+        const t = d.createElement("link"), o = { rel: "stylesheet", type: "text/css", href: e };
+        Object.keys(o).forEach(e => { t.setAttribute(e, o[e]) }), d.querySelector("head").appendChild(t)
+    });
+    a.map(t => {
+        const e = d.createElement("script"), o = { type: "text/javascript", src: t };
+        Object.keys(o).forEach(t => { e.setAttribute(t, o[t]) }), d.body.appendChild(e);
+    });
+    sel.map((e, a) => {
+        const i = d.createElement("div"); i.classList.add("iframe"), i.innerHTML = c[a], d.querySelector(e).appendChild(i);
+    });
+}());
