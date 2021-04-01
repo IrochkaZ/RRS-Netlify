ea4d8ebbb8d19286c917bcba29ddcd94cd4aaf63
Iryna  Zaletko <43729239+IrochkaZ@users.noreply.github.com>
2021-04-01 15:19:01 +0300

    Update loader.js

--- data/loader.js
+++ data/loader.js
@@ -1,10 +1,15 @@
+
 const loader = document.getElementById('order-public');
 const selector = loader.getAttribute('selector');
 
 if (loader && selector) {
-    const URL = 'https://zaochnik1.com/static/public/', d = document,
-        nam = (loader.getAttribute('name') === 'shortAndLong') ? ['short', 'long'] : [loader.getAttribute('name')],
-        sel = selector.replace(/\s+/g, '').split(','), script = [], link = [], c = [];
+    const URL = 'https://zaochnik1.com/static/public/';
+    const d = document;
+    const nam = (loader.getAttribute('name') === 'shortAndLong') ? ['short', 'long'] : [loader.getAttribute('name')];
+    const selector = selector.replace(/\s+/g, '').split(',');
+    const script = [];
+    const link = [];
+    const c = [];
     nam.map((n) => {
         const data = {
             links: [`${URL}0.min.css`, `${URL}client_order_public_${n === 'short' ? 'short_blog_' : ''}iframe.min.css`],
@@ -15,16 +20,24 @@ if (loader && selector) {
         data.scripts.map(s => script.push(s));
         c.push(data.component);
     })
-    const a = [...new Set(script)], l = [...new Set(link)];
-    l.map(e => {
-        const t = d.createElement("link"), o = { rel: "stylesheet", type: "text/css", href: e };
-        Object.keys(o).forEach(e => { t.setAttribute(e, o[e]) }), d.querySelector("head").appendChild(t)
+    const scriptTotal = [...new Set(script)];
+    const linksTotal = [...new Set(link)];
+    linksTotal.map(item => {
+        const link = d.createElement("link");
+        const opt = { rel: "stylesheet", type: "text/css", href: item };
+        Object.keys(opt).forEach(item => { link.setAttribute(item, opt[item]) });
+        d.querySelector("head").appendChild(link);
     });
-    a.map(t => {
-        const e = d.createElement("script"), o = { type: "text/javascript", src: t };
-        Object.keys(o).forEach(t => { e.setAttribute(t, o[t]) }), d.body.appendChild(e);
+    scriptTotal.map(item => {
+        const script = d.createElement("script");
+        const opt = { type: "text/javascript", src: item };
+        Object.keys(opt).forEach(t => { script.setAttribute(t, opt[item]) });
+        d.body.appendChild(script);
     });
-    sel.map((e, a) => {
-        const i = d.createElement("div"); i.classList.add("iframe"), i.innerHTML = c[a], d.querySelector(e).appendChild(i);
+    selector.map((item, index) => {
+        const iframe = d.createElement("div");
+        iframe.classList.add("iframe");
+        iframe.innerHTML = c[index];
+        d.querySelector(item).appendChild(iframe);
     });
 }
